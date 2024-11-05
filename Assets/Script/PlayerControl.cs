b135b781f3893326dc534361a5a628ecb85e946d
Pyorang <sudangi143@gmail.com>
2024-11-05 17:59:45 +0900

    프로젝트생성및달리기애니메이션추가

---
+++ Assets/Script/PlayerControl.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private void Update()
+    {
+        //키 입력시 점프
+    }
+}
