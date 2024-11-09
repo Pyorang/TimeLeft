14e56dad3abdc7d03df1170b51ca234da54245da
Pyorang <sudangi143@gmail.com>
2024-11-09 16:37:24 +0900

    공격애니메이션및공격과점프추가

---
+++ Assets/Script/PlayerAnimation.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void TriggerAttackAnimation() { anim.SetTrigger("Attack"); }
+}
